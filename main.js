09886bf006a8d032f6bef9779d11fe7dc4687674
Davi <131999650+DaviPac@users.noreply.github.com>
2025-08-21 12:28:50 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,48 @@
+const listaJogos = [
+    {name: "Termo", link: "https://term.ooo"},
+    {name: "Angle", link: "https://angle.wtf"},
+    {name: "Globle", link: "https://globle-game.com/"},
+    {name: "Nerdle", link: "https://nerdlegame.com/"},
+    {name: "Flagle", link: "https://www.flagle.io/"},
+    {name: "Conexo", link: "https://conexo.ws/pt/"}
+];
+
+const jogos = JSON.parse(localStorage.getItem("jogos") || "[]");
+if (!jogos || !jogos[0] || !jogos[0].name) {
+    listaJogos.forEach(jogo => {
+        jogos.push({name: jogo.name, link:jogo.link, state: false});
+    });
+}
+
+function carregarJogos() {
+    jogos.forEach(jogo => {
+        const container = document.getElementById("Jogos");
+        const template = document.getElementById("jogo-template");
+        const clone = template.content.cloneNode(true);
+        const divJogo = clone.querySelector(".jogo");
+        divJogo.dataset.estado = jogo.state ? "resolvido" : "pendente";
+        const botao = clone.querySelector(".botao-jogo");
+        botao.textContent = jogo.name;
+        botao.addEventListener("click", () => {
+            jogo.state = true;
+            localStorage.setItem("jogos", JSON.stringify(jogos));
+            sortJogos();
+            const novaAba = window.open("", "_blank");
+            novaAba.location.href = jogo.link;
+        });
+        container.appendChild(clone);
+    });
+}
+
+sortJogos();
+
+function sortJogos() {
+    jogos.sort((a, b) => {
+        if (a.state === b.state) return 0;
+        if (a.state) return 1;
+        return -1;
+    });
+    const container = document.getElementById("Jogos");
+    container.innerHTML = "";
+    carregarJogos();
+}
